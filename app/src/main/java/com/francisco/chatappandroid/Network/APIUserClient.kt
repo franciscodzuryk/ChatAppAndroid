bbe6fb302427576580082c6f2939bd641d9dc2bd
Bruno Rey <brunomartinrey@gmail.com>
2020-10-06 18:53:50 -0300

    Metodos U-Request

--- app/src/main/java/com/francisco/chatappandroid/Network/APIUserClient.kt
+++ app/src/main/java/com/francisco/chatappandroid/Network/APIUserClient.kt
@@ -3,9 +3,19 @@ package com.francisco.chatappandroid.Network
 import com.francisco.chatappandroid.Model.User
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface APIUserClient {
     @POST("/user/login/")
     fun login(@Body user: User): Call<User>
+
+    @POST(value = "/user//logout/")
+    fun logout(@Body user: User): Call<User>
+
+    @POST(value = "/user//message/")
+    fun sendMessage(@Body user: User): Call<User>
+
+    @GET(value = "/user//message/")
+    fun getMessage(@Body user: User): Call<User>
 }
